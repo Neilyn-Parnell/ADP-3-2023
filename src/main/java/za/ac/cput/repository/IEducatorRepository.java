85086560ae51788ab24bead36a45a8c9f213f4a1
Neilyn-Parnell <neilynparnell@gmail.com>
2023-06-15 21:35:50 +0200

    removed redundant files

--- src/main/java/za/ac/cput/repository/IEducatorRepository.java
+++
@@ -1,9 +0,0 @@
-package za.ac.cput.repository;
-
-import za.ac.cput.domain.Educator;
-
-import java.util.Set;
-
-public interface IEducatorRepository extends IRepository<Educator, String> {
-    public Set<Educator> getAll();
-}
